e0f7cb6f0a0c9865774ca84cc1e0d502543e4cdc
Irfankhan132 <irfanee125@gmail.com>
2025-08-25 20:24:08 +0200

    add vue js project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png">
+<HelloWorld msg="Welcome to Irfan's life with a lot of success, fauiler, struggle and achivement" />
+<HomeView />
+<TwoWayBinding />
+<GetInputValues />
+<CheckboxRadio />
+<IfElse />
+<ParentChildComponent />
+
+</template>
+
+<script>
+import CheckboxRadio from './components/CheckboxRadio.vue';
+import GetInputValues from './components/GetInputValues.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './components/HomeView.vue'
+import IfElse from './components/ifElse.vue';
+import ParentChildComponent from './components/ParentChildComponent.vue';
+import TwoWayBinding from './components/TwoWayBinding.vue'
+
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+        HomeView,
+        TwoWayBinding,
+        GetInputValues,
+        CheckboxRadio,
+        IfElse,
+        ParentChildComponent,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ParentChildComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>How to pass value in child component from parent component</h2>
+    <h2>{{ name2 }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h2>{{ user.email }}</h2>
+</template>
+
+<script>
+
+// 1. Make CHild Component.
+// 2. Import and use CHild Component
+// 3. Pass Static Data
+// 4. Pass Property, object, and function to child Component
+
+export default{
+
+    name: "ParentChildComponent",
+    props:{
+        name2:String,
+        user:Object
+    }
+
+}
+
+
+</script>
+
+<style>
+h1{
+
+    color: brown;
+}
+
+
+</style>
